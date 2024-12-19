6d49721362bdf03e23472482bafba5b178a5c61c
Johannes <johannes-programming@mailfence.com>
2024-12-19 17:00:46 +0100

    u

--- docs/script.js
+++ docs/script.js
@@ -5,22 +5,23 @@ function updateHead() {
     addStylesheet();
 }
 function addMetaTag() {
+    const existing = document.querySelector('meta[charset="UTF-8"]');
+    if(existing){return;}
     const metaTag = document.createElement("meta"); // Create a new meta element
     metaTag.setAttribute("charset", "UTF-8"); // Set its charset attribute
     document.head.appendChild(metaTag); // Append it to the head
 }
 function addViewport(){
-    // Create a new meta element
+    const existing = document.querySelector('meta[name="viewport"]');
+    if(existing){return;}
     const meta = document.createElement('meta');
-
-    // Set the attributes for the meta tag
     meta.name = "viewport";
     meta.content = "width=device-width, initial-scale=1.0";
-
-    // Append the meta tag to the head section
     document.head.appendChild(meta);
 }
 function addStylesheet() {
+    const existing = document.querySelector('link[rel="stylesheet"]');
+    if(existing){return;}
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = 'https://www.johannes-programming.online/styles.css';
