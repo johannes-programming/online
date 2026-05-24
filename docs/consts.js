fb60d44babdd67972a39a19ce9c38e8228234dac
Johannes <johannes-programming@mailfence.com>
2026-05-24 22:54:18 +0200

    u

--- docs/consts.js
+++ docs/consts.js
@@ -14,6 +14,7 @@ export function getProjectNames() {
         'identityfunction',
         'iterflat',
         'iterprod',
+        'jacobus',
         'keyalias',
         'makeprop',
         'na_quantors',
